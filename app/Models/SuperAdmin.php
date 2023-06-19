72b8e1b63b1ff2828d659eb48f24d4c9cb85942d
Nguemoue <lucchuala@kamix.io>
2023-06-19 18:48:44 +0100

    arrangement des fichiers de factory

--- app/Models/SuperAdmin.php
+++ app/Models/SuperAdmin.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class superAdmin extends Authenticatable
+class SuperAdmin extends Authenticatable
 {
-	use Notifiable;
+	use Notifiable,HasFactory;
 	function connections(): \Illuminate\Database\Eloquent\Relations\MorphMany
 	{
 		return $this->morphMany(Connection::class,"profile");
