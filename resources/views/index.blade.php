4250d25a8e7a3a67a1821f92c67a63b0ab88c43b
Nguemoue <lucchuala@kamix.io>
2023-06-27 07:02:32 +0100

    bugfix de la route post pour les permissions

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,34 +5,47 @@
 		<h2 class="text-underline">Page D'acceuil</h2>
 	</div>
 	<hr>
-    <div class="row gap-1  border" style="gap:4px 0px;font-family: 'Algerian',serif;letter-spacing: 2px;font-size: 5em;font-weight: bolder">
-        <div class="col-6">
-            <a href="{{ route('scan.index') }}" style="line-height: 100px;" class="w-100 btn btn-outline-success">
-                <span class="fa fa-folder-open fa-2x"></span>
-                Scanner les documents
-            </a>
-        </div>
-        <div class="col-6">
-            <a href="{{ route('traitement.index') }}" style="line-height: 100px;" class="w-100 btn btn-outline-secondary">
-                <span class="fa fa-pencil fa-2x"></span>
-                Effectuer un traitement
-            </a>
-        </div>
-    </div>
-    <hr class="my-4">
-    <div class="row">
+	<div class="row gap-1  border"
+		  style="gap:4px 0px;font-family: 'Algerian',serif;letter-spacing: 2px;font-size: 5em;font-weight: bolder">
+		@can(config('perm_names.SCAN_DOC'))
+			<div class="col-6">
+				<a href="{{ route('scan.index') }}" style="line-height: 100px;" class="w-100 btn btn-outline-success">
+					<span class="fa fa-folder-open fa-2x"></span>
+					Scanner les documents
+				</a>
+			</div>
+		@endcan
+		@can(config('perm_names.TRAIT_DOC'))
+			<div class="col-6">
+				<a href="{{ route('traitement.index') }}" style="line-height: 100px;"
+					class="w-100 btn btn-outline-secondary">
+					<span class="fa fa-pencil fa-2x"></span>
+					Effectuer un traitement
+				</a>
+			</div>
+		@endcan
 
-        <div class="col-6">
-            <a href="{{ route('navigation.index') }}" style="line-height: 100px;" class="w-100 btn btn-outline-info">
-                <span class="fa fa-navicon fa-2x"></span>
-                Navigation
-            </a>
-        </div>
-        <div class="col-6">
-            <a href="{{ route('statistique.index') }}" style="line-height: 100px;" class="w-100 btn btn-outline-danger">
-                <span class="fa fa-book fa-2x"></span>
-                Statistiques
-            </a>
-        </div>
-    </div>
+	</div>
+	<hr class="my-4">
+	<div class="row">
+		@can(config('perm_names.NAV'))
+
+			<div class="col-6">
+				<a href="{{ route('navigation.index') }}" style="line-height: 100px;" class="w-100 btn btn-outline-info">
+					<span class="fa fa-navicon fa-2x"></span>
+					Navigation
+				</a>
+			</div>
+		@endcan
+		@can(config('perm_names.SHOW_STAT'))
+
+			<div class="col-6">
+				<a href="{{ route('statistique.index') }}" style="line-height: 100px;" class="w-100 btn btn-outline-danger">
+					<span class="fa fa-book fa-2x"></span>
+					Statistiques
+				</a>
+			</div>
+		@endcan
+
+	</div>
 @endsection
